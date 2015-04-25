80755f027cf3ced00bb0f4940c00aa95fc093b7c
Walker Christie <walker781@yahoo.com>
2015-04-25 18:51:58 -0400

    successfully dequeueable

--- Pinsit/TappedPinLikesCell.swift
+++ Pinsit/TappedPinLikesCell.swift
@@ -15,8 +15,16 @@ class TappedPinLikesCell: UITableViewCell {
     override init(style: UITableViewCellStyle, reuseIdentifier: String?) {
         super.init(style: style, reuseIdentifier: reuseIdentifier)
     }
-
+    
     required init(coder aDecoder: NSCoder) {
         super.init(coder: aDecoder)
+    }
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+    }
+    
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
     }
 }
