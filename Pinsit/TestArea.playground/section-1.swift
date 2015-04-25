548aaf8863f69c1c0e5f2695ff7265ee095368d5
Walker Christie <walker781@yahoo.com>
2015-04-25 18:52:44 -0400

    Playground tests

--- Pinsit/TestArea.playground/section-1.swift
+++ Pinsit/TestArea.playground/section-1.swift
@@ -1,8 +1,7 @@
 // Playground - noun: a place where people can play
 
+import Foundation
 import UIKit
 
-var label = UILabel(frame: CGRectMake(0, 0, 500, 100))
-label.backgroundColor = UIColor(red: 255/255, green: 41/255, blue: 81/255, alpha: 1)
-//label.layer.shadowColor = UIColor.blackColor().CGColor
+let path = NSBundle.mainBundle()
 
