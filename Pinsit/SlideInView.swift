61c6ee960d2014244f929d991daf4d837011c80f
Walker Christie <walker781@yahoo.com>
2015-05-27 23:41:24 -0400

    WIP Slide in object

---
+++ Pinsit/SlideInView.swift
@@ -0,0 +1,43 @@
+//
+//  SlideUpView.swift
+//  Pinsit
+//
+//  Created by Walker Christie on 5/25/15.
+//  Copyright (c) 2015 Walker Christie. All rights reserved.
+//
+
+import Foundation
+
+class SlideInView: UIView {
+    private let screenHeight = UIScreen.mainScreen().bounds.height
+    var slideSpeed: NSTimeInterval = 0.5
+    var inDisplay: Bool = false
+    
+    func slideIn(doneAnimating: (() -> Void)?) {
+        if inDisplay == true {
+            doneAnimating!()
+            return
+        }
+
+        self.frame.origin.y = screenHeight
+        UIView.animateWithDuration(slideSpeed, animations: { () -> Void in
+            self.frame.origin.y = self.screenHeight - self.frame.size.height
+        }) { (done) -> Void in
+            self.inDisplay = true
+            
+            if doneAnimating != nil {
+                doneAnimating!()
+            }
+        }
+    }
+    
+    func dismissSlide() {
+        if inDisplay == false {
+            return
+        }
+        
+        UIView.animateWithDuration(slideSpeed, animations: { () -> Void in
+            self.frame.origin.y = self.screenHeight
+        }, completion: nil)
+    }
+}
