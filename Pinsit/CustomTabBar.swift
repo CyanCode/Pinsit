257aa94bcf19a12e31763f572f8f25e14c2f777e
Walker Christie <walker781@yahoo.com>
2015-07-08 01:33:44 -0400

    Deprecation accommodations for UITabBar

--- Pinsit/CustomTabBar.swift
+++ Pinsit/CustomTabBar.swift
@@ -10,19 +10,20 @@ import UIKit
 
 class CustomTabBar: UITabBarController {
     override func viewDidLoad() {
-        tabBar.barTintColor = UIColor(string: "#252B2D")
-        tabBar.tintColor = UIColor(string: "#FF2951")
-        tabBar.selectedImageTintColor = UIColor(string: "#FF2951")
+        tabBar.barTintColor = UIColor(string: "#252B2D") //Dark-Gray
+        tabBar.tintColor = UIColor(string: "#A3BDC6") //Lightblue-white
+        tabBar.selectedImageTintColor = UIColor(string: "#A3BDC6") //Lightblue-white
+        //Old pink-red: FF2951
         
         //Unselected text color
         let unselected = UIColor.whiteColor()
         let colorDict: NSDictionary = [NSForegroundColorAttributeName: UIColor.whiteColor()]
-        
-        UITabBarItem.appearance().setTitleTextAttributes(colorDict as [NSObject : AnyObject], forState: .Normal)
-        UITabBarItem.appearance().setTitleTextAttributes(colorDict as [NSObject : AnyObject], forState: .Selected)
+
+        //UITabBarItem.appearance().setTitleTextAttributes(colorDict as [NSObject : AnyObject], forState: UIControlState.Normal)
+        //UITabBarItem.appearance().setTitleTextAttributes(colorDict as [NSObject : AnyObject], forState: .Selected)
         
         //Unselected image color
-        for item in tabBar.items as! [UITabBarItem] {
+        for item in tabBar.items! as [UITabBarItem] {
             item.image = item.image?.imageWithRenderingMode(UIImageRenderingMode.AlwaysOriginal)
         }
         
