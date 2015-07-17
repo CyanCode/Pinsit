1404e876dfefde521837c4960f70819503f09e4d
Walker Christie <walker781@yahoo.com>
2015-07-16 22:46:00 -0400

    Displays error notification at top of screen

---
+++ Pinsit/ErrorReport.swift
@@ -0,0 +1,35 @@
+//
+//  ErrorReport.swift
+//  Pinsit
+//
+//  Created by Walker Christie on 7/16/15.
+//  Copyright © 2015 Walker Christie. All rights reserved.
+//
+
+import Foundation
+
+class ErrorReport {
+    var viewController: UIViewController!
+    
+    init(viewController: UIViewController) {
+        self.viewController = viewController
+    }
+    
+    init() {
+        viewController = UIViewController.topMostController()
+    }
+    
+    func presentError(title: String, message: String, type: TSMessageNotificationType) {
+        TSMessage.showNotificationInViewController(viewController, title: title, subtitle: message, type: type)
+    }
+    
+    func presentWithType(type: ErrorReportType) {
+        switch type {
+        case .Network: presentError("Network Error", message: "Could not connect to the internet.  Check your network connection!", type: .Error)
+        }
+    }
+}
+
+enum ErrorReportType {
+    case Network
+}
