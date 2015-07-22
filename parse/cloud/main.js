f55d985dc75e06d242b6c996887aff224c910103
Walker Christie <walker781@yahoo.com>
2015-07-22 13:08:55 -0400

    Twill API updated

--- parse/cloud/main.js
+++ parse/cloud/main.js
@@ -1,21 +1,19 @@
 Parse.Cloud.define("verifyNumber", function(request, response) {
-    var twilio = require("twilio");
-    twilio.initialize("AC90b7d2008703ddfb9c8acc3274c1d924", "9968159162bcf90e1c63c2860c0a0847");
-  
-    twilio.sendSMS({
-        From: "+16178198676",
-        To: request.params.number,
-        Body: "Welcome to Pinsit!  Your verification code is " + request.params.verification
-    }, {
-        success: function(httpResponse) {
-            response.success("SMS Sent");
-        },
-        error: function(httpResponse) {
-            response.error("Something went wrong");
-        }
-    });
+    var twilio = require("twilio")("AC90b7d2008703ddfb9c8acc3274c1d924", "9968159162bcf90e1c63c2860c0a0847");
+   
+    twilio.sendSms({
+        to: request.params.number,
+		from: "+17814793999",
+        body: "Welcome to Pinsit!  Your verification code is " + request.params.verification
+    }, function (err, responseData) {
+		if (err) {
+			response.error(err)
+		} else {
+			response.success();
+		}
+	});
 });
-  
+   
 Parse.Cloud.define("setUserPhone", function(request, response) {
     var query = new Parse.Query(Parse.User);
     query.equalTo("objectId", request.params.userId);
@@ -31,15 +29,15 @@ Parse.Cloud.define("setUserPhone", function(request, response) {
         }
     })
 });
-  
+   
 Parse.Cloud.job("deleteOldPins", function(request, status) {
     var PinObject = Parse.Object.extend("SentData");
     var query = new Parse.Query(PinObject);
     var day = new Date();
-  
+   
     day.setDate(day.getDate() - 1);
     query.lessThan("createdAt", day);
-  
+   
     query.find({
         success: function(results) {
             Parse.Object.destroyAll(results);
