6b42ae6f2539e006eab37d6a5798f9f208e1d84d
Walker Christie <walker781@yahoo.com>
2015-08-22 00:54:45 -0400

    - Added ability to change verification email source - Email regex verification added

--- Pinsit/RecoverAccount.swift
+++ Pinsit/RecoverAccount.swift
@@ -22,26 +22,38 @@ class RecoverAccount {
     }
     
     func reverifyEmailAddress(completion: () -> Void) {
-        Email().isEmailVerified({ (confirmed) -> Void in
-            if confirmed == true {
-                Email.emailVerifiedMessage()
-                completion()
-            } else {
-                Email().resendVerification({ (error) -> Void in
+        let controller = UIAlertController(title: "Verify Email Address", message: "What email would you like to verify?", preferredStyle: .Alert)
+        controller.addTextFieldWithConfigurationHandler { (textField) -> Void in
+            textField.placeholder = "Email Address"
+            textField.keyboardType = .EmailAddress
+        }
+        controller.addAction(UIAlertAction(title: "Cancel", style: .Cancel, handler: nil))
+        controller.addAction(UIAlertAction(title: "Verify", style: .Default, handler: { (action) -> Void in
+            let text = (controller.textFields![0] as! UITextField).text
+            PFUser.currentUser()!.email = text
+            
+            var regex = ".+@([A-Za-z0-9]+\\.)+[A-Za-z]{2}[A-Za-z]*";
+            var test = NSPredicate(format:"SELF MATCHES \(regex)");
+            
+            if test.evaluateWithObject(text) {
+                self.sendRecoveryEmail(text, done: { (error) -> Void in
                     if error != nil {
-                        let controller = UIAlertController(title: "Not Quite..", message: "We were not able to send a verification email, check your connection and try again!", preferredStyle: .Alert)
-                        controller.addAction(UIAlertAction(title: "Okay", style: .Cancel, handler: nil))
-                        self.viewController.presentViewController(controller, animated: true, completion: nil)
-                        completion()
+                        ErrorReport(viewController: self.viewController).presentError("Uh Oh..", message: "We were unable to send you a verification email, check your connection and try again!", type: .Error)
                     } else {
-                        let controller = UIAlertController(title: "Success", message: "Your recovery email has been sent, please check your email!", preferredStyle: .Alert)
-                        controller.addAction(UIAlertAction(title: "Okay", style: .Cancel, handler: nil))
-                        self.viewController.presentViewController(controller, animated: true, completion: nil)
-                        completion()
+                        ErrorReport(viewController: self.viewController).presentError("Great!", message: "Your verification email has been sent to \(text)", type: .Error)
                     }
                 })
+            } else {
+                ErrorReport(viewController: self.viewController).presentError("Something's Missing", message: "The email you entered is not valid, try entering a different one!", type: .Warning)
             }
-        })
+        }))
+    }
+    
+    func sendRecoveryEmail(email: String, done: (error: NSError?) -> Void) {
+        PFUser.currentUser()!.email = email
+        PFUser.currentUser()!.saveInBackgroundWithBlock { (success, error) -> Void in
+            done(error: error)
+        }
     }
     
     func recoverPassword(email: String, completion: (error: NSError?) -> Void) {
